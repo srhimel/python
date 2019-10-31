639659c5692cb152a52e6ec05c0de9a04d3e6a23
shahamar rahman <himusr@gmail.com>
2019-11-01 00:51:56 +0600

    2 new python project

---
+++ loopsum.py
@@ -0,0 +1,6 @@
+num = 0
+result = 1
+for result in range(50):
+    result = num + result
+    num += 1
+print(result)
---
+++ multiplicationtable.py
@@ -0,0 +1,6 @@
+n = input("Please input a positive integer: ")
+n = int(n)
+m = 1
+while m <= 10:
+    print(n, "x", m, "=", n*m)
+    m +=1
