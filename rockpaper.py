2a04b05f72121172d3cee4fcdd9418e22ec58f1f
shahamar rahman <himusr@gmail.com>
2020-01-05 09:25:03 +0600

    limit input option

--- rockpaper.py
+++ rockpaper.py
@@ -8,26 +8,30 @@ for i in range(5):
     cochoice = option[pos]
 
     mychoic = input("What is your choice between: rock , paper , scissors? ")
-    if mychoic==cochoice:
-        print("You choose:", mychoic)
-        print("Computer choose:", cochoice)
-        print("Result: It's drawn")
-        draw += 1
-    elif mychoic=="rock" and cochoice=="paper":
-        print("You choose:", mychoic)
-        print("Computer choose:", cochoice)
-        print("Result: Computer Won")
-        lost += 1
-    elif mychoic == "paper" and cochoice == "scissors":
-        print("You choose:", mychoic)
-        print("Computer choose:", cochoice)
-        print("Result: Computer Won")
-        lost += 1
+    if mychoic=="rock" or mychoic=="paper" or mychoic=="scissors":
+        if mychoic==cochoice:
+            print("You choose:", mychoic)
+            print("Computer choose:", cochoice)
+            print("Result: It's drawn")
+            draw += 1
+        elif mychoic=="rock" and cochoice=="paper":
+            print("You choose:", mychoic)
+            print("Computer choose:", cochoice)
+            print("Result: Computer Won")
+            lost += 1
+        elif mychoic == "paper" and cochoice == "scissors":
+            print("You choose:", mychoic)
+            print("Computer choose:", cochoice)
+            print("Result: Computer Won")
+            lost += 1
+        else:
+            print("You choose:", mychoic)
+            print("Computer choose:", cochoice)
+            print("Result: You Won")
+            win +=1
     else:
-        print("You choose:", mychoic)
-        print("Computer choose:", cochoice)
-        print("Result: You Won")
-        win +=1
+        print("Only allowed inputs are: rock,paper,scissors")
+        exit()
 print("Draw: ",draw ,"Win: ",win ,"Lost: ",lost)
 
 if win==draw==lost:
@@ -36,4 +40,3 @@ elif(win<lost):
     print("You looser")
 else:
     print("Winner! Winner!! Chicken Dinner!!!")
-
